23ef44809002cf1c7c326b3059fbd0f192d2a3f4
WillChew <williamchew95@gmail.com>
2018-06-17 22:03:33 -0400

    before auto

---
+++ Bill Splitter/Bill Splitter/ViewController.m
@@ -0,0 +1,84 @@
+//
+//  ViewController.m
+//  Bill Splitter
+//
+//  Created by Will Chew on 2018-06-17.
+//  Copyright © 2018 Will Chew. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController () <UITextFieldDelegate>
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) IBOutlet UILabel *label;
+@property (weak, nonatomic) IBOutlet UISlider *slider;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.textField.delegate = self;
+    [self setupTextField];
+    [self displayText:@""];
+    
+    UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(screenTapped:)];
+    [self.view addGestureRecognizer:tapGesture];
+    
+    [self.slider addObserver:self selector:@selector(sliderValueChanged:) name:UIControlEventValueChanged object:nil];
+
+}
+- (void)screenTapped:(UITapGestureRecognizer*)sender {
+    [self.textField resignFirstResponder];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+- (IBAction)calculateSplitAmount:(UIButton *)sender {
+    [self.textField resignFirstResponder];
+    
+}
+- (IBAction)sliderValueChanged:(id)sender {
+    float newValue = self.slider.value;
+    NSLog(@"%.2f", newValue);
+}
+
+
+-(void)setupTextField {
+    self.textField.keyboardType = UIKeyboardTypeNumberPad;
+}
+
+-(void)displayText:(NSString*)text {
+//    int billAmount = self.textField.text.intValue;
+    float splitTotal = self.textField.text.floatValue / self.slider.value;
+    
+    NSNumberFormatter *numberFormatter = [[NSNumberFormatter alloc]init];
+    [numberFormatter setNumberStyle:NSNumberFormatterCurrencyAccountingStyle];
+    
+    NSString *sliderAsStr = [numberFormatter stringFromNumber:[NSNumber numberWithFloat:splitTotal]];
+    
+    self.label.text = sliderAsStr;
+//    NSLog(@"%@", decimal);
+    
+}
+
+- (void)textFieldDidBeginEditing:(UITextField *)textField {
+    [self displayText:@""];
+    
+}
+
+-(void)textFieldDidEndEditing:(UITextField *)textField {
+    [self displayText:self.textField.text];
+}
+
+-(BOOL)textFieldShouldClear:(UITextField *)textField {
+    [self displayText:@""];
+    return YES;
+}
+
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string 
+
+@end
